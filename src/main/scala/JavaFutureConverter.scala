fc4c4f66d0c1b234c3c0fa64fe092d31284292ec
Yoshimura Yuu <m@yyu.pw>
2017-06-08 14:36:29 +0900

    use Scala Future and Future#transform instead of Promise (thanks @gakuzzzz)

--- src/main/scala/JavaFutureConverter.scala
+++ src/main/scala/JavaFutureConverter.scala
@@ -1,27 +1,16 @@
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.{Future => JavaFuture}
 import scala.concurrent.ExecutionContext
-import scala.concurrent.Promise
 import scala.concurrent.{Future => ScalaFuture}
 import scala.util.Failure
-import scala.util.Try
 
 object JavaFutureConverter {
   def toScala[A](jf: JavaFuture[A])(implicit ec: ExecutionContext): ScalaFuture[A] = {
-    val p: Promise[A] = Promise[A]
-
-    ec.execute { () =>
-      p.complete(
-        Try(jf.get()) match {
-          case Failure(e: ExecutionException) =>
-            Failure(e.getCause)
-          case x =>
-            x
-        }
-      )
+    ScalaFuture(jf.get()).transform {
+      case Failure(e: ExecutionException) =>
+        Failure(e.getCause)
+      case x => x
     }
-
-    p.future
   }
 
   implicit class RichJavaFuture[A](val jf: JavaFuture[A]) extends AnyVal {
